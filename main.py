5eece5c51d0903783234cd63caea031fd9d9dfed
Jamily Vitoria <86215917+jmysvt@users.noreply.github.com>
2023-05-23 23:37:50 -0300

    Primeira parte do menu

---
+++ main.py
@@ -0,0 +1,15 @@
+print("-" * 54)
+print(
+  "Seja bem vindo ao nosso menu, One Week At The Python! Com base no seu número inteiro, escolha a opção que desejar!!"
+)
+print("-" * 54)
+
+numint = (int(input("Digite um número inteiro:")))
+
+print("-" * 54)
+print("""Escolha um dos seguintes cálculos:
+[1] Conversão de decimal para as bases binário e octadecimal.
+[2] Conversão das bases binário e octadecimal para decimal.
+[3] Calculadora aritmética de binários, que contemple  as operações de soma e subtração."""
+      )
+print("-" * 54)
